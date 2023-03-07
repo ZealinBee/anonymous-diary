94f319aa2a34b267d2a4c98a97a27db3fe69156a
Zhiyuan Liu <thenukezealot@gmail.com>
2023-03-07 11:33:46 +0200

    production ready

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,7 +7,6 @@ const Diary = require("../model/diary");
 router.get("/diaries", async (req, res) => {
   try {
     const entries = await Diary.find();
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app');
     res.status(200).json(entries);
   } catch (err) {
@@ -23,7 +22,6 @@ router.post("/diaries", async (req, res) => {
   });
   try {
     const newEntry = await entry.save();
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app/');
     res.status(201).json({ newEntry });
   } catch (err) {
