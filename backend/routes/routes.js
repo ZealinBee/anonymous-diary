7b89a6033973e8cc42895c65c6eeb83a4612301f
Zhiyuan Liu <thenukezealot@gmail.com>
2023-03-07 11:19:20 +0200

    CORS

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,6 +8,7 @@ router.get("/diaries", async (req, res) => {
   try {
     const entries = await Diary.find();
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app/');
     res.status(200).json(entries);
   } catch (err) {
     return res.status(500).json({ message: err.message });
@@ -23,6 +24,7 @@ router.post("/diaries", async (req, res) => {
   try {
     const newEntry = await entry.save();
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app/');
     res.status(201).json({ newEntry });
   } catch (err) {
     return res.status(500).json({ message: err.message });
