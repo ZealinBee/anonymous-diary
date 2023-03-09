ffb6096cff518e5924d85a4c9ba507142edd8183
Zhiyuan Liu <thenukezealot@gmail.com>
2023-03-09 17:05:00 +0200

    vite configuration

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,8 +7,7 @@ const Diary = require("../model/diary");
 router.get("/diaries", async (req, res) => {
   try {
     const entries = await Diary.find();
-    // res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', `${process.env.CORS_URL}`);
     res.status(200).json(entries);
   } catch (err) {
     return res.status(500).json({ message: err.message });
@@ -24,8 +23,7 @@ router.post("/diaries", async (req, res) => {
   });
   try {
     const newEntry = await entry.save();
-    // res.setHeader('Access-Control-Allow-Origin', 'https://anonymous-diary-fullstack.up.railway.app/');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', `${process.env.CORS_URL}`);
     res.status(201).json({ newEntry });
   } catch (err) {
     return res.status(500).json({ message: err.message });
